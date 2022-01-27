d54ebef3c9aff0408aaf5ec32be20d27c4ef2185
Kira <mukeshreddy.9.new@gmail.com>
2022-01-27 19:30:07 +0530

    feat: made tag command admin only

--- src/commands/updateTag.js
+++ src/commands/updateTag.js
@@ -9,31 +9,39 @@ module.exports = new Command({
     description: "Updates an existing tag",
 
     async run(msg, args, client) {
-        const messageArray = args.slice(2, args.length)
-        const tagName = args[1]
-        const message = messageArray.join(" ")
-
-        if (args.length >= 3) {
-            tags.findOneAndUpdate({ name: tagName }, { message: message }, function (err, data) {
-                if (data) {
-                    const successEmbed = new Discord.MessageEmbed()
-                        .setDescription("Successfully updated the tag!")
-                        .setColor(success)
-                    msg.reply({ embeds: [successEmbed] })
-                }
-                else {
-                    const errorEmbed = new Discord.MessageEmbed()
-                        .setDescription(`Didn't find any tag named \`${tagName}\``)
-                        .setColor(error)
-                    msg.reply({ embeds: [errorEmbed] })
-                }
-            })
+        if (msg.author.flags.bitfield !== 8) {
+            const noPermEmbed = new Discord.MessageEmbed()
+                .setDescription("<:the_bonk:843682837210202122> Nice try, but I won't accept you try tho")
+                .setColor(error)
+            msg.reply({ embeds: [noPermEmbed] })
         }
         else {
-            const errorEmbed = new Discord.MessageEmbed()
-                .setDescription("Invalid number of arguments")
-                .setColor(error)
-            msg.reply({ embeds: [errorEmbed] })
+            const messageArray = args.slice(2, args.length)
+            const tagName = args[1]
+            const message = messageArray.join(" ")
+
+            if (args.length >= 3) {
+                tags.findOneAndUpdate({ name: tagName }, { message: message }, function (err, data) {
+                    if (data) {
+                        const successEmbed = new Discord.MessageEmbed()
+                            .setDescription("Successfully updated the tag!")
+                            .setColor(success)
+                        msg.reply({ embeds: [successEmbed] })
+                    }
+                    else {
+                        const errorEmbed = new Discord.MessageEmbed()
+                            .setDescription(`Didn't find any tag named \`${tagName}\``)
+                            .setColor(error)
+                        msg.reply({ embeds: [errorEmbed] })
+                    }
+                })
+            }
+            else {
+                const errorEmbed = new Discord.MessageEmbed()
+                    .setDescription("Invalid number of arguments")
+                    .setColor(error)
+                msg.reply({ embeds: [errorEmbed] })
+            }
         }
     }
 });
